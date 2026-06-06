2b1cdf59319dd2ffb9299b43a3df14e6575beba3
LCherryholmes <lcherryh@yahoo.com>
2026-06-06 23:13:26 +0000

    pascal: PB-18 add stdlib2.pas probe (sin/cos/sqrt/arctan/exp/ln)

---
+++ programs/pascal/stdlib2.pas
@@ -0,0 +1,16 @@
+program stdlib2(output);
+var r : real;
+begin
+  r := sin(1.0);
+  writeln(r);
+  r := cos(0.0);
+  writeln(r);
+  r := sqrt(2.0);
+  writeln(r);
+  r := arctan(1.0);
+  writeln(r);
+  r := exp(1.0);
+  writeln(r);
+  r := ln(r);
+  writeln(r)
+end.
