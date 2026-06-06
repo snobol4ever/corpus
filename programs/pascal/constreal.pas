37e8ecbc53cec369577c36ca28ae030adc76e61d
LCherryholmes <lcherryh@yahoo.com>
2026-06-06 23:47:52 +0000

    pascal: PB-21 add constreal.pas probe (real/string named constants)

---
+++ programs/pascal/constreal.pas
@@ -0,0 +1,15 @@
+program constreal(output);
+const
+  pi = 3.14159265;
+  msg = 'ok';
+  limit = 100;
+var r : real; i : integer;
+begin
+  r := pi;
+  writeln(r);
+  writeln(msg);
+  i := limit;
+  writeln(i);
+  r := -pi;
+  writeln(r)
+end.
