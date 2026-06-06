393f8c37806d8ceec9d9cf818eb13831fb29ade1
LCherryholmes <lcherryh@yahoo.com>
2026-06-06 23:21:55 +0000

    pascal: PB-19 add realwidth.pas probe (write(r:w) width specifier)

---
+++ programs/pascal/realwidth.pas
@@ -0,0 +1,12 @@
+program realwidth(output);
+var r : real;
+begin
+  r := sqrt(2.0);
+  writeln(r:20);
+  writeln(r:15);
+  writeln(r:12);
+  writeln(r:10);
+  r := arctan(1.0);
+  writeln(r:20);
+  writeln(r:15)
+end.
