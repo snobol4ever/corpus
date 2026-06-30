45b6fec34dd2b4b29442d97e08ffac0284af06ee
LCherryholmes <lcherryh@yahoo.com>
2026-06-30 13:19:22 +0000

    icon bench asm: regen version.s after TO/TO BY/every codegen change (honest current output)

--- benchmarks/icon/version.s
+++ benchmarks/icon/version.s
@@ -21,28 +21,45 @@ push r12
   mov r12, rdi
   lea r10, [rip + Δ]
 main_α_body:
+xchain0_n0_α:
+# IR_KEYWORD_read
 bb1_α:
-# BOX IR_CALL write(...) -> rt_call_arr [operand-marshal, FAIL->ω]
-# marshal arg0 = nested producer-box slot [r12+16] -> [r12+16]
- mov rax, qword ptr [r12 + 16]
- mov qword ptr [r12 + 16], rax
- mov rax, qword ptr [r12 + 24]
- mov qword ptr [r12 + 24], rax
-  .section .rodata
-  .Lrkfn1: .string "write"
-  .section .text
-  .intel_syntax noprefix
-   lea rdi, [rip + .Lrkfn1]
- lea rsi, [r12 + 16]
- mov edx, 1
- call rt_call_arr@PLT
+ mov rdi, qword ptr [rip + .Lx1_0]
+ call rt_keyword_read@PLT
  mov qword ptr [r12 + 0], rax
  mov qword ptr [r12 + 8], rdx
+ jmp xchain0_n1_α
+ xchain0_n0_β:
+ jmp main_ω
+.Lx1_0:
+ .quad .Lx1_0_s
+.Lx1_0_s:
+ .string "&version"
+xchain0_n1_α:
+bb2_α:
+# BOX IR_CALL write(...) -> rt_call_arr [operand-marshal, FAIL->ω]
+# marshal arg0 = nested producer-box slot [r12+0] -> [r12+32]
+ mov rax, qword ptr [r12 + 0]
+ mov qword ptr [r12 + 32], rax
+ mov rax, qword ptr [r12 + 8]
+ mov qword ptr [r12 + 40], rax
+  .section .rodata
+  .Lrkfn3: .string "write"
+  .section .text
+  .intel_syntax noprefix
+   lea rdi, [rip + .Lrkfn3]
+ lea rsi, [r12 + 32]
+ mov edx, 1
+ call rt_call_arr@PLT
+ mov qword ptr [r12 + 16], rax
+ mov qword ptr [r12 + 24], rdx
  cmp eax, 99
  je main_ω
  jmp main_γ
- main_β:
+ xchain0_n1_β:
  jmp main_ω
+main_β:
+jmp main_ω
 main_γ:
 mov eax, 1
 xor edx, edx
