3259d44a60754aa3abe5c988234dc5b138c14ed9
LCherryholmes <lcherryh@yahoo.com>
2026-07-11 19:19:58 +0000

    benchmark x86 .s artifacts: NCB-1c M3

--- benchmarks/snobol4/pattern_bt.s
+++ benchmarks/snobol4/pattern_bt.s
@@ -816,9 +816,36 @@ main_α_body:
  push rbp
  mov rbp, rsp
  and rsp, -16
- call rt_dcap_end_ok@PLT
  mov rsp, rbp
  pop rbp
+ push r14
+ push r15
+ push r13
+ push rbp
+ mov rbp, rsp
+ and rsp, -16
+ call rt_dcap_end_ok_open@PLT
+.Lx58_1:
+ test rax, rax
+ je .Lx58_2
+ sub rsp, rax
+ mov rdi, rsp
+ mov rsi, rax
+ call rt_frame_prep@PLT
+ mov rdi, rsp
+ xor esi, esi
+ call rax
+ mov rdi, rax
+ mov rsi, rdx
+ call rt_dcap_step@PLT
+ jmp .Lx58_1
+.Lx58_2:
+ call rt_dcap_end_ok_close@PLT
+ mov rsp, rbp
+ pop rbp
+ pop r13
+ pop r15
+ pop r14
  jmp xchain25_n26_α
 # IR_VAR
  xchain25_n25_α:
--- benchmarks/snobol4/string_pattern.s
+++ benchmarks/snobol4/string_pattern.s
@@ -957,9 +957,36 @@ main_α_body:
  push rbp
  mov rbp, rsp
  and rsp, -16
- call rt_dcap_end_ok@PLT
  mov rsp, rbp
  pop rbp
+ push r14
+ push r15
+ push r13
+ push rbp
+ mov rbp, rsp
+ and rsp, -16
+ call rt_dcap_end_ok_open@PLT
+.Lx62_1:
+ test rax, rax
+ je .Lx62_2
+ sub rsp, rax
+ mov rdi, rsp
+ mov rsi, rax
+ call rt_frame_prep@PLT
+ mov rdi, rsp
+ xor esi, esi
+ call rax
+ mov rdi, rax
+ mov rsi, rdx
+ call rt_dcap_step@PLT
+ jmp .Lx62_1
+.Lx62_2:
+ call rt_dcap_end_ok_close@PLT
+ mov rsp, rbp
+ pop rbp
+ pop r13
+ pop r15
+ pop r14
  jmp xchain9_n43_α
 # IR_LIT_STRING
  xchain9_n43_α:
