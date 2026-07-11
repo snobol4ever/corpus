c97d1a4051585c0aba23a7c48bda37153e018fb5
LCherryholmes <lcherryh@yahoo.com>
2026-07-11 18:58:03 +0000

    x64 demo artifacts: NCB-1c M1

--- programs/snobol4/demo/wordcount.s
+++ programs/snobol4/demo/wordcount.s
@@ -538,13 +538,40 @@ main_α_body:
 .Lx32_1:
  jmp xchain5_n25_α
 .Lx32_0:
+ push r14
+ push r15
+ push r13
  lea rdi, [rip + .S1]
  xor esi, esi
- mov edx, r14d
  push rbp
  mov rbp, rsp
  and rsp, -16
- call rt_defer_match@PLT
+ call rt_defer_open@PLT
+.Lx32_2:
+ test rax, rax
+ je .Lx32_3
+ sub rsp, rax
+ mov rdi, rsp
+ mov rsi, rax
+ call rt_frame_prep@PLT
+ mov rdi, rsp
+ xor esi, esi
+ call rax
+ mov rdi, rax
+ mov rsi, rdx
+ call rt_defer_step@PLT
+ jmp .Lx32_2
+.Lx32_3:
+ mov rsp, rbp
+ pop rbp
+ pop r13
+ pop r15
+ pop r14
+ mov edi, r14d
+ push rbp
+ mov rbp, rsp
+ and rsp, -16
+ call rt_defer_close@PLT
  mov rsp, rbp
  pop rbp
  test eax, eax
