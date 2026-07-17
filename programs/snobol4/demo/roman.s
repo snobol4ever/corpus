ef90da877d8e1179fb9791d67bf4e3a810e11a72
LCherryholmes <lcherryh@yahoo.com>
2026-07-17 19:43:02 +0000

    x64 demo artifacts: regen

--- programs/snobol4/demo/roman.s
+++ programs/snobol4/demo/roman.s
@@ -538,12 +538,12 @@ jmp main_ω
 main_γ:
 mov eax, 1
 xor edx, edx
-mov rsp, qword ptr [rsp + 632]
+mov rsp, qword ptr [rbp + 632]
 mov rbp, [rsp + 65536]
 add rsp, 65544
 ret
 main_ω:
-mov rsp, qword ptr [rsp + 632]
+mov rsp, qword ptr [rbp + 632]
 mov dword ptr [rsp+0], 99
 mov dword ptr [rsp+4], 0
 mov qword ptr [rsp+8], 0
--- programs/snobol4/demo/wordcount.s
+++ programs/snobol4/demo/wordcount.s
@@ -725,12 +725,12 @@ jmp main_ω
 main_γ:
 mov eax, 1
 xor edx, edx
-mov rsp, qword ptr [rsp + 1096]
+mov rsp, qword ptr [rbp + 1096]
 mov rbp, [rsp + 65536]
 add rsp, 65544
 ret
 main_ω:
-mov rsp, qword ptr [rsp + 1096]
+mov rsp, qword ptr [rbp + 1096]
 mov dword ptr [rsp+0], 99
 mov dword ptr [rsp+4], 0
 mov qword ptr [rsp+8], 0
