d6382711aa05a757f6a81e55fba3cc0d5973b4f4
LCherryholmes <lcherryh@yahoo.com>
2026-07-17 19:17:47 +0000

    x64 demo artifacts: regen

--- programs/snobol4/demo/roman.s
+++ programs/snobol4/demo/roman.s
@@ -432,16 +432,16 @@ pop rbp
 proc_ROMAN_β:
 jmp proc_ROMAN_ω
 proc_ROMAN_γ:
-mov rdi, [rsp]
-mov rsi, [rsp + 8]
-mov rax, [rsp + 648]
+mov rdi, [rbp]
+mov rsi, [rbp + 8]
+mov rax, [rbp + 648]
+lea rsp, [rbp + 672]
 mov rbp, [rbp + 664]
-lea rsp, [rsp + 672]
 jmp rax
 proc_ROMAN_ω:
-mov rax, [rsp + 656]
+mov rax, [rbp + 656]
+lea rsp, [rbp + 672]
 mov rbp, [rbp + 664]
-lea rsp, [rsp + 672]
 jmp rax
 proc_startup:
   sub rsp, 8
@@ -472,6 +472,9 @@ proc_startup:
   lea rdi, [rip + .Lstartup_pname0]
   mov esi, 640
   call rt_proc_set_frame_bytes@PLT
+  lea rdi, [rip + .Lstartup_pname0]
+  mov esi, 1
+  call rt_proc_set_jmpentry@PLT
   add rsp, 8
   ret
   .section .rodata
--- programs/snobol4/demo/wordcount.s
+++ programs/snobol4/demo/wordcount.s
@@ -126,6 +126,9 @@ proc_startup:
   lea rdi, [rip + .Lstartup_pname0]
   mov esi, 144
   call rt_proc_set_frame_bytes@PLT
+  lea rdi, [rip + .Lstartup_pname0]
+  mov esi, 1
+  call rt_proc_set_jmpentry@PLT
   add rsp, 8
   ret
   .section .rodata
