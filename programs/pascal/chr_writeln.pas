e5355f049512565b3a05418ce5f17080951dca10
LCherryholmes <lcherryh@yahoo.com>
2026-07-19 21:55:00 +0000

    pascal: gate chr_writeln probe (PAS-SELFHOST-3c chr/writeln char formatting)

---
+++ programs/pascal/chr_writeln.pas
@@ -0,0 +1,10 @@
+program t;
+var c : char; i : integer;
+begin
+  writeln(chr(112));
+  writeln(chr(65), chr(66), chr(67));
+  c := chr(88);
+  writeln(c, c);
+  i := ord(chr(90));
+  writeln(i:1)
+end.
