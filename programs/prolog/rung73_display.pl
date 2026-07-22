e3146d02715644559d54301b10b533f4b75f9714
LCherryholmes <lcherryh@yahoo.com>
2026-07-22 23:52:21 +0000

    pl: rung73_display -- display/1,/2 + print/2 + .expected oracle (gprolog 1.4.5)

---
+++ programs/prolog/rung73_display.pl
@@ -0,0 +1,21 @@
+:- initialization(main).
+main :-
+    display(1+2*3), nl,
+    display((1+2)*3), nl,
+    display([a,b,c]), nl,
+    display('hello world'), nl,
+    display('Foo'), nl,
+    display('.'), nl,
+    display(foo(bar(1),baz(2))), nl,
+    display((a:-b,c)), nl,
+    display(- 5), nl,
+    T1 = f(X,Y,g(X,Z)), numbervars(T1, 0, _), display(T1), nl,
+    T2 = [p,q|W], numbervars(T2, 0, _), display(T2), nl,
+    display(user_output, hello(world)), nl,
+    print(1+2*3), nl,
+    print([a,b,c]), nl,
+    print('hello world'), nl,
+    print(foo(bar(1),baz(2))), nl,
+    print(user_output, [x,y,z]), nl,
+    write(done), nl,
+    halt.
