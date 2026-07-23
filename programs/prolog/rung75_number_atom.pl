c7475f2bc8c5d3df51bcc3ca6c6d74a27c876d5c
LCherryholmes <lcherryh@yahoo.com>
2026-07-23 00:04:50 +0000

    pl: rung75_number_atom + .expected (gprolog 1.4.5)

---
+++ programs/prolog/rung75_number_atom.pl
@@ -0,0 +1,12 @@
+:- initialization(main).
+main :-
+    number_atom(123, A1), writeq(A1), nl,
+    number_atom(-42, A2), writeq(A2), nl,
+    number_atom(0, A3), writeq(A3), nl,
+    number_atom(N1, '456'), ( integer(N1) -> writeq(N1-int) ; writeq(N1-other) ), nl,
+    number_atom(N2, '-7'), writeq(N2), nl,
+    number_atom(N3, '2.5'), ( float(N3) -> writeq(N3-flt) ; writeq(N3-other) ), nl,
+    number_atom(2.5, A4), writeq(A4), nl,
+    ( number_atom(789, '789') -> writeq(roundtrip_ok) ; writeq(roundtrip_fail) ), nl,
+    write(done), nl,
+    halt.
