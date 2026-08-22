f716381439335ecf74b28a6ec84541710f101616
LCherryholmes <lcherryh@yahoo.com>
2026-08-22 17:13:14 -0500

    x64 demo artifacts: regen

--- programs/snobol4/demo/counter.s
+++ programs/snobol4/demo/counter.s
@@ -157,12 +157,9 @@ n13_cmp_test_α:         sub              rsp, 16
                         cmp              rax, rdx;                            jl    .Lx40_239
                         add              rsp, 16;                             jmp   n12_coerce_numeric_β
 .Lx40_239:                                                                    jmp   n14_var_α
-.Lx40_0:                lea              rdi, [rsp + 32]
-                        lea              rsi, [rsp + 16]
-                        mov              qword ptr [rip + rtccb+40], r8
+.Lx40_0:                lea              rdi, [rsp + 32]                      # a
+                        lea              rsi, [rsp + 16]                      # b
                         call             rt_cmp_d@PLT
-                        mov              r8,  qword ptr [rip + rtccb+40]
-                        mov              r9,  qword ptr [rip + rtccb+48]
                         test             eax, eax;                            js    .Lx40_240
                         add              rsp, 16;                             jmp   n12_coerce_numeric_β
 .Lx40_240:                                                                    jmp   n14_var_α
