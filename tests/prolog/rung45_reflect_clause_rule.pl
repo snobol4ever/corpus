398d47cd175785c77dbeedab3f9f6206a2829256
LCherryholmes <lcherryh@yahoo.com>
2026-08-28 09:25:47 -0500

    prolog tests: convert rung45_reflect_clause_rule (1 of 6 entries; other 5 stay loose, PZ-4-blocked) to suite format

--- tests/prolog/rung45_reflect_clause_rule.pl
+++ tests/prolog/rung45_reflect_clause_rule.pl
@@ -1,2 +1,3 @@
+%-------------------------------------------------- 1 rung45_reflect_clause_rule
 main :- assertz((greet :- write(hello), nl)),
         ( clause(greet, Body) -> call(Body) ; write(noclause), nl ).
