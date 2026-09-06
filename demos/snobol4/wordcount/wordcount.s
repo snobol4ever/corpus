150a0b1c00dcaeb8d1903d6c49171a1bf3230394
LCherryholmes <lcherryh@yahoo.com>
2026-09-06 09:46:03 -0500

    x64 demo artifacts: regen

--- demos/snobol4/wordcount/wordcount.s
+++ demos/snobol4/wordcount/wordcount.s
@@ -176,7 +176,12 @@ main:
                         call             rtcc_load_all@PLT
                         xor              esi, esi
                         xor              r14d, r14d
+                        lea              rax, [rip + .Llevel_zero_return]
+                        push             rax
+                        push             rax
                                                                               jmp   main_α
+.Llevel_zero_return:    call             rt_kw_return_level_zero@PLT
+                        ud2
                         .section         .rodata
 .Lgvan0:                .string          "NUMERALS"
 .Lgvan1:                .string          "WORD"
