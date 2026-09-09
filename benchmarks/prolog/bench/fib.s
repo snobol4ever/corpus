5fb7fc6b73c606601ff8f184db2810207066aba1
LCherryholmes <lcherryh@yahoo.com>
2026-09-09 09:15:05 -0500

    prolog bench x86 .s artifacts: prolog-bench-regen

--- benchmarks/prolog/bench/fib.s
+++ benchmarks/prolog/bench/fib.s
@@ -1,6 +1,6 @@
                         .intel_syntax    noprefix
                         .text
-                        .file            1 "/home/claude_coo/corpus/benchmarks/prolog/bench/fib.pl"
+                        .file            1 "/home/claude_V/corpus/benchmarks/prolog/bench/fib.pl"
                         .file            2 "<included>"
 #-----------------------------------------------------------------------------------------------------------------------
 FN__fib$2F2:
