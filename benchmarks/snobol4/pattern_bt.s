67c189d7f3f8294ee9eef8e7a13bbe1b748a3030
LCherryholmes <lcherryh@yahoo.com>
2026-09-11 15:40:58 -0500

    benchmark x86 .s artifacts: progname-argv0

--- benchmarks/snobol4/pattern_bt.s
+++ benchmarks/snobol4/pattern_bt.s
@@ -226,6 +226,9 @@ main:
                         mov              esi, 4
                         call             rt_label_table_install@PLT
                         mov              rdi, qword ptr [rsp]
+                        mov              rdi, qword ptr [rdi]
+                        call             rt_main_progname_stage@PLT
+                        mov              rdi, qword ptr [rsp]
                         add              rdi, 8
                         mov              esi, dword ptr [rsp + 8]
                         sub              esi, 1
