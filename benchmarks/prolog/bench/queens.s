46e4e4f2c20f3d5ab62c028fb3d614f2081bda4f
LCherryholmes <lcherryh@yahoo.com>
2026-09-12 15:33:08 -0500

    prolog bench x86 .s artifacts: prolog-bench-regen

--- benchmarks/prolog/bench/queens.s
+++ benchmarks/prolog/bench/queens.s
@@ -1,6 +1,6 @@
                         .intel_syntax    noprefix
                         .text
-                        .file            1 "/home/claude_cto/corpus/benchmarks/prolog/bench/queens.pl"
+                        .file            1 "/home/claude_ceo/corpus/benchmarks/prolog/bench/queens.pl"
                         .file            2 "<included>"
 #-----------------------------------------------------------------------------------------------------------------------
 FN__sel$2F3:
