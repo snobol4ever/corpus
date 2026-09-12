af23ce3819e32564428c9887c7a88bad48511abf
LCherryholmes <lcherryh@yahoo.com>
2026-09-12 09:55:24 -0500

    prolog bench x86 .s artifacts: icon-string-builtins-103-and-calc-conversions

--- benchmarks/prolog/bench/queens_8.s
+++ benchmarks/prolog/bench/queens_8.s
@@ -1,6 +1,6 @@
                         .intel_syntax    noprefix
                         .text
-                        .file            1 "/home/claude_cto/corpus/benchmarks/prolog/bench/queens_8.pl"
+                        .file            1 "/home/claude_B/corpus/benchmarks/prolog/bench/queens_8.pl"
                         .file            2 "<included>"
 #-----------------------------------------------------------------------------------------------------------------------
 FN__sel$2F3:
