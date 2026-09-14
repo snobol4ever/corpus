9c250ea7320b6e382274699c38edf831e64a5238
LCherryholmes <lcherryh@yahoo.com>
2026-09-13 19:31:14 -0500

    prolog bench x86 .s artifacts: sn4-computed-lvalue-spine-release

--- benchmarks/prolog/bench/deriv.s
+++ benchmarks/prolog/bench/deriv.s
@@ -1,6 +1,6 @@
                         .intel_syntax    noprefix
                         .text
-                        .file            1 "/home/claude_R/corpus/benchmarks/prolog/bench/deriv.pl"
+                        .file            1 "/home/claude_U/corpus/benchmarks/prolog/bench/deriv.pl"
                         .file            2 "<included>"
 #-----------------------------------------------------------------------------------------------------------------------
 FN__$db_decl$2F0:
