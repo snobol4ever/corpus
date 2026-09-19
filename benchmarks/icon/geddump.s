f181c70b41c6043f5f0f9b5a19d9768fb74c1b5d
LCherryholmes <lcherryh@yahoo.com>
2026-09-19 16:24:55 -0500

    benchmark x86 .s artifacts: section-3 polls at the emitted return

--- benchmarks/icon/geddump.s
+++ benchmarks/icon/geddump.s
@@ -5696,6 +5696,20 @@ n00230_scan_sequence_α:   mov              r11, 275
                         mov              r10, qword ptr [rip + rtccb+56]
                         mov              r11, qword ptr [rip + rtccb+64]
                         add              rsp, 16
+                        sub              rsp, 16
+                        mov              qword ptr [rsp + 0], rax
+                        mov              qword ptr [rsp + 8], rdx
+                        mov              qword ptr [rip + rtccb+40], r8
+                        mov              qword ptr [rip + rtccb+56], r10
+                        mov              qword ptr [rip + rtccb+64], r11
+                        call             rt_gc_poll@PLT
+                        mov              r8,  qword ptr [rip + rtccb+40]
+                        mov              r9,  qword ptr [rip + rtccb+48]
+                        mov              r10, qword ptr [rip + rtccb+56]
+                        mov              r11, qword ptr [rip + rtccb+64]
+                        mov              rax, qword ptr [rsp + 0]
+                        mov              rdx, qword ptr [rsp + 8]
+                        add              rsp, 16
                         mov              qword ptr [rbp + 848], rax
                         mov              qword ptr [rbp + 856], rdx;          jmp   n00235_assign_α
 n00230_scan_sequence_β:   mov              r11, 275
