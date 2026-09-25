c4f7b0257598193a268407f7febe532a3130d321
LCherryholmes <lcherryh@yahoo.com>
2026-09-25 14:56:55 -0500

    icon bench x86 .s artifacts: regenerated on SCRIP be0e10b9a -- the mode-4 main adopts its own huge-page stack first (CEO-1265); 17 updated, 0 refused

--- benchmarks/icon/bench_icnstr_concat_strvar.s
+++ benchmarks/icon/bench_icnstr_concat_strvar.s
@@ -4,6 +4,16 @@
                         .file            2 "<included>"
                         .globl           main
 main:
+                        push             rdi
+                        push             rsi
+                        sub              rsp, 8
+                        call             rt_main_stack_adopt@PLT
+                        mov              rsi, qword ptr [rsp + 8]
+                        mov              rdi, qword ptr [rsp + 16]
+                        add              rsp, 24
+                        test             rax, rax;                            jz    .Lmain_stack_kept
+                        mov              rsp, rax
+.Lmain_stack_kept:
                         sub              rsp, 65544
                         push             rdi
                         push             rsi
@@ -18,7 +28,7 @@ main:
                         add              rdi, 8
                         mov              esi, dword ptr [rsp + 8]
                         sub              esi, 1
-                        call             rt_main_args_stage@PLT
+                        call             rt_main_args_stage_argv@PLT
                         call             rtcc_load_all@PLT
                         xor              esi, esi
                         xor              r14d, r14d
