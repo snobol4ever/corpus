eeb28ce8ec142ce5116436fe125c4085ac9e1280
LCherryholmes <lcherryh@yahoo.com>
2026-09-25 14:56:44 -0500

    prolog bench x86 .s artifacts: prolog-bench-regen

--- benchmarks/prolog/bench/fib.s
+++ benchmarks/prolog/bench/fib.s
@@ -31033,6 +31033,16 @@ clause$2F2_ω:
 .Lgcmap_clause$2F2_s:   .string          "clause/2"
                         .globl           main
 main:
+                        push             rdi
+                        push             rsi
+                        sub              rsp, 8
+                        call             rt_main_stack_adopt@PLT
+                        mov              rsi, qword ptr [rsp + 8]
+                        mov              rdi, qword ptr [rsp + 16]
+                        add              rsp, 24
+                        test             rax, rax;                            jz    .Lmain_stack_kept
+                        mov              rsp, rax
+.Lmain_stack_kept:
                         sub              rsp, 65544
                         push             rdi
                         push             rsi
@@ -31047,7 +31057,7 @@ main:
                         add              rdi, 8
                         mov              esi, dword ptr [rsp + 8]
                         sub              esi, 1
-                        call             rt_main_args_stage@PLT
+                        call             rt_main_args_stage_argv@PLT
                         call             rt_gcheap_warmup@PLT
                         call             rtcc_load_all@PLT
                         xor              esi, esi
--- benchmarks/prolog/bench/qsort.s
+++ benchmarks/prolog/bench/qsort.s
@@ -35135,6 +35135,16 @@ clause$2F2_ω:
 .Lgcmap_clause$2F2_s:   .string          "clause/2"
                         .globl           main
 main:
+                        push             rdi
+                        push             rsi
+                        sub              rsp, 8
+                        call             rt_main_stack_adopt@PLT
+                        mov              rsi, qword ptr [rsp + 8]
+                        mov              rdi, qword ptr [rsp + 16]
+                        add              rsp, 24
+                        test             rax, rax;                            jz    .Lmain_stack_kept
+                        mov              rsp, rax
+.Lmain_stack_kept:
                         sub              rsp, 65544
                         push             rdi
                         push             rsi
@@ -35149,7 +35159,7 @@ main:
                         add              rdi, 8
                         mov              esi, dword ptr [rsp + 8]
                         sub              esi, 1
-                        call             rt_main_args_stage@PLT
+                        call             rt_main_args_stage_argv@PLT
                         call             rt_gcheap_warmup@PLT
                         call             rtcc_load_all@PLT
                         xor              esi, esi
